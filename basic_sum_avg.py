7b0507d57c876d233c5e1fe9a86458cb06332acf
MuhammadWaqas2012 <muhammadwaqas.12.12.2012@gmail.com>
2026-03-29 18:55:25 +0500

    Update basic_sum_avg.py

--- basic_sum_avg.py
+++ basic_sum_avg.py
@@ -1,8 +1,8 @@
-#get input of 1,2 subjects marks
-#calculate total, avg
-subj1 = input("enter you marks in subject 1: ")
-subj2 = input("enter you marks in subject 2: ")
-sum = int(subj1) + int(subj2)
+# sum of two numbers
+num1 = input("enter first number: ")
+num2 = input("enter second number: ")
+sum = num1 + num2
+print("Sum = ", sum)
+# average calculation
 avg = sum/2
-print("Sum is = ", sum)
-print("Average is = ", avg)
+print("Average = ", avg)
