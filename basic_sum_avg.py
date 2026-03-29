2ef333ac356621fe794beb1216c40722653e90cb
MuhammadWaqas2012 <muhammadwaqas.12.12.2012@gmail.com>
2026-03-29 18:50:49 +0500

    Add files via upload

---
+++ basic_sum_avg.py
@@ -0,0 +1,8 @@
+#get input of 1,2 subjects marks
+#calculate total, avg
+subj1 = input("enter you marks in subject 1: ")
+subj2 = input("enter you marks in subject 2: ")
+sum = int(subj1) + int(subj2)
+avg = sum/2
+print("Sum is = ", sum)
+print("Average is = ", avg)
